dd0bcef18ce9c48e702b78123ffc2cc54fca3fce
Luke Van De Weghe <39035228+Gorfeywog@users.noreply.github.com>
2025-06-01 14:23:05 -0500

    Fix whitespace in profile.ps1

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -10,11 +10,11 @@ Set-Alias -Name:eps -Value:"Enter-PsSession"
 function Resolve-Error ($ErrorRecord=$Error[0])
 {
    $ErrorRecord | Format-List * -Force
-   $ErrorRecord.InvocationInfo |Format-List *
+   $ErrorRecord.InvocationInfo | Format-List *
    $Exception = $ErrorRecord.Exception
    for ($i = 0; $Exception; $i++, ($Exception = $Exception.InnerException))
    {   "$i" * 80
-       $Exception |Format-List * -Force
+       $Exception | Format-List * -Force
    }
 }
 function modulize {
